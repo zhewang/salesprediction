a0adf4b540aa701f92ae4a378b70a06f2fbad748
Zhe Wang <zwang.contact@gmail.com>
2014-12-05 02:06:03 -0700

    read user's timeline done

--- fetch_twitter.py
+++ fetch_twitter.py
@@ -1,6 +1,16 @@
 from twython import Twython
 
-twitter = Twython()
-results = twitter.get_user_timeline(user_id='amazondeals')
+APP_KEY = u'4MBVh0blR8toU4TZxPWl0yuwG'
+APP_SECRET = u'ZZe1zSflBXCXJopVyJ1x5uZHuwjXTCMmtDXfDwZL7FNLKbHAtP'
+OAUTH_TOKEN = u'2918875315-1dggMat8yFvQcQEccMqQ1LmPoZhr6j21KI669VY'
+OAUTH_TOKEN_SECRET = u'X7uULxPje1dqx5sfFFNekKMmzHkmNXe9gM5NSfZIxEu9B'
 
-print(results)
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+results = twitter.get_user_timeline(screen_name=u'hp', count='2')
+
+for i in results:
+    print(i['created_at'])
+    print(i['retweet_count'])
+    print(i['text'])
+    print("-------------------")
