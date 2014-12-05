4528c7d40d15edba8bb1627f0483ac95191c357a
Zhe Wang <zwang.contact@gmail.com>
2014-12-05 12:20:27 -0700

    fetch and save data to file

--- fetch_twitter.py
+++ fetch_twitter.py
@@ -1,16 +1,39 @@
 from twython import Twython
+import pickle
 
-APP_KEY = u'4MBVh0blR8toU4TZxPWl0yuwG'
-APP_SECRET = u'ZZe1zSflBXCXJopVyJ1x5uZHuwjXTCMmtDXfDwZL7FNLKbHAtP'
-OAUTH_TOKEN = u'2918875315-1dggMat8yFvQcQEccMqQ1LmPoZhr6j21KI669VY'
-OAUTH_TOKEN_SECRET = u'X7uULxPje1dqx5sfFFNekKMmzHkmNXe9gM5NSfZIxEu9B'
+def GetTimeline(conn, user_id):
+    results = conn.get_user_timeline(screen_name=user_id, count='200', exclude_replies='true')
+    # results = twitter.search(q='@hp', count='5', until='2014-11-20')
 
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    # readFile = open('tData.txt', 'rb')
+    # results = pickle.load(readFile)
 
-results = twitter.get_user_timeline(screen_name=u'hp', count='2')
+    for i in results:
+        print(i['created_at'])
+        print(i['retweet_count'])
+        print(i['text'].encode('utf-8'))
+        print(i['id'])
+        print(i['user']['screen_name'])
+        print("-------------------")
 
-for i in results:
-    print(i['created_at'])
-    print(i['retweet_count'])
-    print(i['text'])
-    print("-------------------")
+    saveFile = open('./data/'+user_id,'wb')
+    pickle.dump(results, saveFile)
+    saveFile.close()
+
+def FetchData(id_list):
+
+    ids = ['hp','eBay']
+
+    APP_KEY = u'4MBVh0blR8toU4TZxPWl0yuwG'
+    APP_SECRET = u'ZZe1zSflBXCXJopVyJ1x5uZHuwjXTCMmtDXfDwZL7FNLKbHAtP'
+    OAUTH_TOKEN = u'2918875315-1dggMat8yFvQcQEccMqQ1LmPoZhr6j21KI669VY'
+    OAUTH_TOKEN_SECRET = u'X7uULxPje1dqx5sfFFNekKMmzHkmNXe9gM5NSfZIxEu9B'
+
+    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+    for uid in ids:
+        GetTimeline(twitter, uid)
+
+if __name__ == '__main__':
+    FetchData('test')
+
